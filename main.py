605a980778f07222a7b137aa73ae49d38f2a7f11
ElzaScerbinska <124616741+ElzaScerbinska@users.noreply.github.com>
2023-03-29 17:06:44 +0000

    beidzot

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 # python3
 
-class Query:
-    def __init__(self, query):
-        self.type = query[0]
-        self.number = int(query[1])
-        self.name = ""
-        if self.type == 'add':
-            self.name = query[2] 
-       
-class QueryProcessor:
-    def __init__(self, bucket_count):
+class HashTable:
+    def __init__(self, bucket_count=10):
         self.bucket_count = bucket_count
         self.buckets = [[] for _ in range(bucket_count)]
         self._prime = 1000000007
+        self._multiplier = 263
 
     def _hash_func(self, s):
         ans = 0
@@ -20,70 +13,49 @@ class QueryProcessor:
             ans = (ans * self._multiplier + ord(c)) % self._prime
         return ans % self.bucket_count
 
-    def add(self, string):
-        hashed = self._hash_func(string)
+    def add(self, key, string):
+        #hashed = self._hash_func(string)
+        hashed = self._hash_func(key)
         bucket = self.buckets[hashed]
-        if string not in bucket:
-            self.buckets[hashed] = [string] + bucket
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                bucket[i] = (key, item)
+                return
+        bucket.append((key, item))
+        #if string not in bucket:
+            #self.buckets[hashed] = [string] + bucket
+        print(self.buckets)
 
+    def delete(self, key):
+        hashed = self._hash_func(key)
+        bucket = self.buckets[hashed]
+        for i in range(len(bucket)):
+            if bucket[i][0] == key:
+                bucket.pop(i)
+                break
 
-    def find(self, string):
-        hashed = self._hash_func(string)
-        for s in self.buckets[hashed]:
-            if s == string:
-                return "yes"
+    def find(self, key):
+        hashed = self._hash_func(key)
+        for item in self.buckets[hashed]:
+            if item[0] == key:
+                return item[1]
+        #hashed = self._hash_func(string)
+        #if string in self.buckets[hashed]:
+            #return self.buckets[hashed][0]
         return "not found"
 
-def read_queries():
-    n = int(input())
-    return [Query(input().split()) for i in range(n)]
+hash_table = HashTable()
+num_queries = int(input())
+output = []
 
-def write_responses(result):
-    #print('\n'.join(result))
-    for r in result:
-        print(r)
+for i in range(num_queries):
+    query = input().split()
+    if query[0] == "add":
+        key, item = query[1], query[2]
+        hash_table.add(key, item)
+    elif query[0] == "del":
+        hash_table.delete(query[1])
+    elif query[0] == "find":
+        output.append(hash_table.find(query[1]))
 
-def process_queries(queries):
-    result = []
-    bucket_count = 5
-    proc = QueryProcessor(bucket_count)
-    for query in queries:
-        if query.type == "add" and query.name is not None:
-            proc.add(query.name)
-        elif query.type == "del" and query.name is not None:
-            proc.delete(query.name)
-        else:
-            res = proc.find(query.name)
-            if res == "yes":
-                result.append(query.name)
-            else:
-                result.append("not found")
-    return result
-
-#def process_queries(queries):
-    #result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    #contacts = []
-    #for cur_query in queries:
-        #if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            #for contact in contacts:
-                #if contact.number == cur_query.number:
-                    #contact.name = cur_query.name
-                    #break
-            #else: # otherwise, just add it
-                #contacts.append(cur_query)
-        #elif cur_query.type == 'del':
-            #for j in range(len(contacts)):
-                #if contacts[j].number == cur_query.number:
-                    #contacts.pop(j)
-                    #break
-        #else:
-            #response = 'not found'
-            #for contact in contacts:
-                #if contact.number == cur_query.number:
-                    #response = contact.name
-                    #break
-            #result.append(response)
-    #return result
+print("\n".join(output))
